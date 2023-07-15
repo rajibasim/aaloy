ef9a08309c9e8665fed09727afeb95c492877373
rajibncr2018 <rajib.ncrts@gmail.com>
2023-07-15 11:09:14 +0530

    property added

--- app/Http/Controllers/Application/API/PropertyController.php
+++ app/Http/Controllers/Application/API/PropertyController.php
@@ -230,38 +230,6 @@ class PropertyController extends Controller
         try {
 
             $user_data = Auth::user();
-
-            if (!$request->title) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->gender) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->property_for) {
-                throw new \Exception('This field required');
-            }
-
-            $request->posted_by = $user_data->type;
-            $request->posted_by_id = $user_data->id;
-
-            if (!$request->property_type_id) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->preferance) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->location_id) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->address) {
-                throw new \Exception('This field required');
-            }
-
             if (!$request->latitude) {
                 throw new \Exception('This field required');
             }
@@ -270,147 +238,53 @@ class PropertyController extends Controller
                 throw new \Exception('This field required');
             }
 
-            if (!$request->pin_code) {
+            if (!$request->type) {
                 throw new \Exception('This field required');
             }
 
-            if (!$request->floor_id) {
-                throw new \Exception('This field required');
+            $sql = "SELECT property.id, property.title, property.description, property.property_for, IF(property.property_for = '1', 'Rent', 'Sell') AS property_for_text, property.posted_by, IF(property.posted_by = '1', 'Broker', 'Owner') AS posted_by_txt, property.posted_by_id, users.name as posted_by_name, property.property_type_id, property_type.property_type, property.location_id, location.location, property.address, property.is_address_visible, property.furnishing_status_id, furnishing_status.furnishing_status, property.positioning_status_id, positioning_status.positioning_status, property.car_parking_id, car_parking.car_parking, property.preferance, property.carpet_area, property.floor, property.out_of_floor, property.bathroom, property.no_of_room_id, no_of_room.no_of_room, property.price, property.booking_price, property.maintenance, IF(property.gender = '1', 'Male', IF(property.gender = '2', 'Female', IF(property.gender = '3', 'Transgender', 'No Choice'))) AS gender, property.note, property.is_phone_visible, property.is_email_visible, property.property_image, property.avalible_from, property.is_admin_aproved, property.status, property.video_url, property.created_at, property.updated_at FROM `property` LEFT JOIN users ON property.posted_by_id = users.id LEFT JOIN property_type ON property.property_type_id = property_type.id LEFT JOIN location ON property.location_id = location.id LEFT JOIN furnishing_status ON property.furnishing_status_id = furnishing_status.id LEFT JOIN positioning_status ON property.positioning_status_id = positioning_status.id LEFT JOIN car_parking ON property.car_parking_id = car_parking.id LEFT JOIN no_of_room ON property.no_of_room_id = no_of_room.id WHERE property.status = '1'"; 
+
+            $page_no = 1;
+            if($request->page_no){
+                $page_no = $request->page_no;
             }
 
-            if (!$request->out_of_floor_id) {
-                throw new \Exception('This field required');
+            $per_page = 10;
+            if($request->per_page){
+                $per_page = $request->per_page;
             }
 
-            if (!$request->no_of_room_id) {
-                throw new \Exception('This field required');
+            $page_no = ($page_no-1)*$per_page;
+
+            $total_count = DB::select($sql);
+            $total_count = count($total_count);
+
+            $sql = $sql ." LIMIT ".$page_no.', '.$per_page;
+            $list = DB::select($sql);
+            if (empty($list)) {
+                throw new \Exception('No records found.');
             }
 
-            if (!$request->price) {
-                throw new \Exception('This field required');
-            }
-            if (!$request->booking_price) {
-                throw new \Exception('This field required');
-            }
+            $listArray = [];
+            foreach ($list as $key => $value) {
 
-            if (!$request->maintenance) {
-                throw new \Exception('This field required');
-            }
+                $is_saved = $this->CommonModel->get_all($table = 'users_saved_property', $select = array('id'), $where = array(array('is_deleted', '=', 0), array('property_id', '=', $value->id), array('user_id', '=', $user_data->id)), $join = array(), $left = array(), $right = array(), $order = array(), $group = "", $limit = array(), $raw = "", $paging = "");
+                $value->is_saved = !empty($is_saved) ? 1 : 0;
 
-            if (!$request->carpet_area) {
-                throw new \Exception('This field required');
+                $property_images = $this->CommonModel->get_all($table = 'property_image', $select = array('*'), $where = array(array('is_deleted', '=', 0), array('property_id', '=', $value->id)), $join = array(), $left = array(), $right = array(), $order = array(), $group = "", $limit = array(), $raw = "", $paging = "");
+                $value->property_images = !empty($property_images) ? $property_images : [];
+
+                
+                $listArray[] = $value;
             }
 
-            if (!$request->car_parking_id) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->furnishing_status_id) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->positioning_status_id) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->description) {
-                throw new \Exception('This field required');
-            }
-
-            /*if (!$request->property_image) {
-                throw new \Exception('This field required');
-            }*/
-
-            if (!$request->avalible_from) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->bathroom) {
-                throw new \Exception('This field required');
-            }
-
-            if (!$request->video_url) {
-                throw new \Exception('This field required');
-            } 
-
-            $request->status = 2; 
-
-            $post_data = array(
-                'title' => $request->title,
-                'gender' => $request->gender,
-                'property_for' => $request->property_for,
-                'posted_by' => $request->posted_by,
-                'posted_by_id' => $request->posted_by_id,
-                'property_type_id' => $request->property_type_id,
-                'preferance' => $request->preferance,
-                'location_id' => $request->location_id,
-                'address' => $request->address,
-                'latitude' => $request->latitude,
-                'longitude' => $request->longitude,
-                'pin_code' => $request->pin_code,
-                'floor' => $request->floor_id,
-                'out_of_floor' => $request->out_of_floor_id,
-                'no_of_room_id' => $request->no_of_room_id,
-                'price' => $request->price,
-                'booking_price' => $request->booking_price,
-                'maintenance' => $request->maintenance,
-                'carpet_area' => $request->carpet_area,
-                'car_parking_id' => $request->car_parking_id,
-                'furnishing_status_id' => $request->furnishing_status_id,
-                'positioning_status_id' => $request->positioning_status_id,
-                'note' => $request->note,
-                'description' => $request->description,
-                'avalible_from' => $request->avalible_from,
-                'bathroom' => $request->bathroom,
-                'video_url' => $request->video_url,
-                'status' => $request->status,
-                'slug' => Str::slug($request->title),
-                'updated_at' => date('Y-m-d H:i:s'),
-                'created_at' => date('Y-m-d H:i:s'),
-            );
-
-            $destinationPath = 'public/uploads/property_image/';
-            if (!empty($_FILES)) {
-                if ($_FILES['property_image'] && $_FILES['property_image']['name'] != "") {
-                    $profile_image_new_name = str_replace(" ", "_", time() . $_FILES['property_image']['name']);
-                    if (move_uploaded_file($_FILES['property_image']['tmp_name'], $destinationPath . $profile_image_new_name)) {
-                        $post_data['property_image'] = $destinationPath . $profile_image_new_name;
-                    }
-                }
-            }
-
-            $property_id = $this->CommonModel->insert_data_get_id('property', $post_data); 
-            if($property_id){
-                if(isset($_FILES["multi_image"]["tmp_name"]) && is_array($_FILES["multi_image"]["tmp_name"])){
-                    foreach ($_FILES["multi_image"]["tmp_name"] as $key => $tmp_name) {
-                        $file_name = str_replace(" ", "_", time() . $_FILES["multi_image"]["name"][$key]);
-                        $file_tmp = $file_tmp = $_FILES["multi_image"]["tmp_name"][$key];
-
-                        $target_path = $destinationPath . $file_name;
-                        move_uploaded_file($file_tmp, $target_path);
-
-                        $uploadedImages = [];
-                        $uploadedImages = array(
-                            'property_id' => $property_id,
-                            'property_image' => $target_path,
-                        );
-
-                        $result = $this->CommonModel->insert_data_get_id('property_image', $uploadedImages);
-                    }
-                }
-
-                return response()->json([
-                    'result' => true,
-                    'message' => "Property added Successfully.",
-                    'data' => [],
-                ]);
-            }else{
-                return response()->json([
-                    'result' => false,
-                    'message' => "Something wrong, try again later.",
-                    'data' => [],
-                ]);
-            }                   
+            return response()->json([
+                'result' => true,
+                'message' => '',
+                'data' => $listArray,
+                'total_count' => $total_count,
+            ],200,[],JSON_NUMERIC_CHECK);
+               
         } catch (\Exception $e) {
             return response()->json([
                 'result' => false,
